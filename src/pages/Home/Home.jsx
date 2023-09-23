ad19cf2b3de523bdfe5c9bb868526e13fb2a0a2f
amit joshi <84657391+amit-joshi-sys@users.noreply.github.com>
2023-09-23 15:06:30 +0530

    branding changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
-import styles from './styles.module.css'
-import config from '../../config.json'
+import React, { useEffect, useState } from "react";
+import styles from "./styles.module.css";
+import config from "../../config.json";
 
 const Home = () => {
-  const page = config.ordering.find(page => page.path === '/')
+  const page = config.ordering.find((page) => page.path === "/");
+  const [activeBannerConfig, setActiveBannerConfig] = useState({
+    text: "Exceptional Home Remodeling and Renovations",
+    image: "/images/home_bg_1.jpg",
+    id: 1,
+  });
+
+  const bannerConfig = [
+    {
+      text: "Exceptional Home Remodeling and Renovations",
+      image: "/images/home_bg_1.jpg",
+      id: 1,
+    },
+    {
+      text: "Great Interiors",
+      image: "/images/home_bg_2.jpg",
+      id: 2,
+    },
+  ];
+
   return (
     <div id={page.id} className={styles.container}>
-      Home
+      <div className={styles.content}>
+        <img
+          id="carouselImage"
+          className={styles.carouselImage}
+          src={activeBannerConfig.image}
+          alt={activeBannerConfig.text}
+        />
+        <h1 className={styles.carouselText}>{activeBannerConfig.text}</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,33 +1,54 @@
-import React, { useEffect } from 'react'
-import Home from './Home/Home'
-import OurProjects from './OurProjects/OurProjects'
-import AboutUs from './AboutUs/AboutUs'
-import ContactUs from './ContactUs/ContactUs'
-import styles from './styles.module.css'
-import { useLocation } from 'react-router-dom'
-import config from '../config.json'
-import SideMenu from './SideMenu/SideMenu'
-import Header from './Header/Header'
+import React, { useEffect } from "react";
+import Home from "./Home/Home";
+import OurProjects from "./OurProjects/OurProjects";
+import AboutUs from "./AboutUs/AboutUs";
+import ContactUs from "./ContactUs/ContactUs";
+import styles from "./styles.module.css";
+import { useLocation } from "react-router-dom";
+import config from "../config.json";
+import SideMenu from "./SideMenu/SideMenu";
+import Header from "./Header/Header";
 
 const Pages = () => {
-    const location = useLocation()
+  const location = useLocation();
 
-    useEffect(() => {
-        const pageToScroll = config.ordering.find(page => page.path === location.pathname)
-        const pageElement = document.getElementById(pageToScroll.id)
-        pageElement.scrollIntoView({behavior: 'smooth'})
-    }, [location])
+  useEffect(() => {
+    const pageToScroll = config.ordering.find(
+      (page) => page.path === location.pathname
+    );
+    const pageElement = document.getElementById(pageToScroll.id);
+    pageElement.scrollIntoView({ behavior: "smooth" });
+  }, [location]);
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/":
+        document.title = "Aroh Housing";
+        break;
+      case "/our-projects":
+        document.title = "Aroh Housing | Projects";
+        break;
+      case "/about-us":
+        document.title = "Aroh Housing | About US";
+        break;
+      case "/contact-us":
+        document.title = "Aroh Housing | Contact Us";
+        break;
+      default:
+        break;
+    }
+  }, [location]);
 
   return (
     <div className={styles.mainContainer}>
-        <Header/>
-        <SideMenu/>
-        <Home/>
-        <OurProjects/>
-        <AboutUs/>
-        <ContactUs/>
+      <Header />
+      <SideMenu />
+      <Home />
+      <OurProjects />
+      <AboutUs />
+      <ContactUs />
     </div>
-  )
-}
+  );
+};
 
-export default Pages
+export default Pages;
