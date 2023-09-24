0f51bd7b1757d3d96a8666f5bf54b0c253f93a48
amit joshi <84657391+amit-joshi-sys@users.noreply.github.com>
2023-09-24 17:11:27 +0530

    image rendering bug fix

--- src/pages/OurProjects/OurProjects.jsx
+++ src/pages/OurProjects/OurProjects.jsx
@@ -12,40 +12,37 @@ const OurProjects = () => {
     {
       projectName: "Baner",
       projectLocation: "Baner",
-      thumbnail:
-        "/aroh_housing/images/baner_site_photos/baner_site_thumbnail.jpg",
+      thumbnail: "/images/baner_site_photos/baner_site_thumbnail.jpg",
       photos: [
-        "/aroh_housing/images/baner_site_photos/baner_site_thumbnail.jpg",
-        "/aroh_housing/images/baner_site_photos/baner_site_photo1.jpg",
-        "/aroh_housing/images/baner_site_photos/baner_site_photo2.jpg",
-        "/aroh_housing/images/baner_site_photos/baner_site_photo3.jpg",
-        "/aroh_housing/images/baner_site_photos/baner_site_photo4.jpg",
+        "/images/baner_site_photos/baner_site_thumbnail.jpg",
+        "/images/baner_site_photos/baner_site_photo1.jpg",
+        "/images/baner_site_photos/baner_site_photo2.jpg",
+        "/images/baner_site_photos/baner_site_photo3.jpg",
+        "/images/baner_site_photos/baner_site_photo4.jpg",
       ],
     },
     {
       projectName: "Karve Nagar",
       projectLocation: "Karve Nagar",
-      thumbnail:
-        "/aroh_housing/images/karve_nagar_site_photos/karve_nagar_thumbnail.jpg",
+      thumbnail: "/images/karve_nagar_site_photos/karve_nagar_thumbnail.jpg",
       photos: [
-        "/aroh_housing/images/karve_nagar_site_photos/karve_nagar_thumbnail.jpg",
-        "/aroh_housing/images/karve_nagar_site_photos/karve_nagar_photo1.jpg",
-        "/aroh_housing/images/karve_nagar_site_photos/karve_nagar_photo2.jpg",
-        "/aroh_housing/images/karve_nagar_site_photos/karve_nagar_photo3.jpg",
-        "/aroh_housing/images/karve_nagar_site_photos/karve_nagar_photo4.jpg",
+        "/images/karve_nagar_site_photos/karve_nagar_thumbnail.jpg",
+        "/images/karve_nagar_site_photos/karve_nagar_photo1.jpg",
+        "/images/karve_nagar_site_photos/karve_nagar_photo2.jpg",
+        "/images/karve_nagar_site_photos/karve_nagar_photo3.jpg",
+        "/images/karve_nagar_site_photos/karve_nagar_photo4.jpg",
       ],
     },
     {
       projectName: "Ghorpadi",
       projectLocation: "Ghorpadi",
-      thumbnail:
-        "/aroh_housing/images/ghorpadi_site_photos/ghorpadi_site_thumbnail.jpg",
+      thumbnail: "/images/ghorpadi_site_photos/ghorpadi_site_thumbnail.jpg",
       photos: [
-        "/aroh_housing/images/ghorpadi_site_photos/ghorpadi_site_thumbnail.jpg",
-        "/aroh_housing/images/ghorpadi_site_photos/ghorpadi_site_photo1.jpg",
-        "/aroh_housing/images/ghorpadi_site_photos/ghorpadi_site_photo2.jpg",
-        "/aroh_housing/images/ghorpadi_site_photos/ghorpadi_site_photo3.jpg",
-        "/aroh_housing/images/ghorpadi_site_photos/ghorpadi_site_photo4.jpg",
+        "/images/ghorpadi_site_photos/ghorpadi_site_thumbnail.jpg",
+        "/images/ghorpadi_site_photos/ghorpadi_site_photo1.jpg",
+        "/images/ghorpadi_site_photos/ghorpadi_site_photo2.jpg",
+        "/images/ghorpadi_site_photos/ghorpadi_site_photo3.jpg",
+        "/images/ghorpadi_site_photos/ghorpadi_site_photo4.jpg",
       ],
     },
   ];
