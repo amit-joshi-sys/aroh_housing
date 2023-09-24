a688d006e59eb678dd61e34fdf66153d494ffc10
amit joshi <84657391+amit-joshi-sys@users.noreply.github.com>
2023-09-24 13:40:15 +0530

    bug fix of deploy issue

--- src/router.js
+++ src/router.js
@@ -1,24 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
-import Pages from './pages'
+import Pages from "./pages";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Pages/>,
-        children: [
-            {
-                path: 'our-projects',
-                element: <Pages/>
-            },
-            {
-                path: 'about-us',
-                element: <Pages/>
-            },
-            {
-                path: 'contact-us',
-                element: <Pages/>
-            }
-        ]
-    },
+  {
+    path: "/aroh_housing",
+    element: <Pages />,
+    children: [
+      {
+        path: "our-projects",
+        element: <Pages />,
+      },
+      {
+        path: "about-us",
+        element: <Pages />,
+      },
+      {
+        path: "contact-us",
+        element: <Pages />,
+      },
+    ],
+  },
 ]);
-
